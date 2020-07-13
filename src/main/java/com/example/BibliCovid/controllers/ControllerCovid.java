45d8740c68882f340bcee93c36d7fcd56765f6b7
bdportilla1 <bryantporti@gmail.com>
2020-07-13 12:51:56 -0500

    Creacion de la vista index

---
+++ src/main/java/com/example/BibliCovid/controllers/ControllerCovid.java
@@ -0,0 +1,18 @@
+package com.example.BibliCovid.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ControllerCovid {
+
+	
+	
+	@RequestMapping("/")
+	public String home(Model model) {
+		String vista="index";
+		return vista;
+	}
+}
+
